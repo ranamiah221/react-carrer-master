2d617d8118dc15cfe3807691cfb9c866595e7439
ranamiah221 <ranarasul21@gmail.com>
2024-10-14 12:32:27 +0600

    aaded responsive for small device in banner page

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,15 +1,15 @@
 import bannerImg from '../../assets/images/user.png'
 const Banner = () => {
     return (
-        <div className='flex items-center mb-10 '>
-              <div className='w-1/2 py-14'>
-                   <h1 className='text-6xl font-bold mb-5'>One Step <br /> Closer For Your <br /> <span className='text-blue-500'>Dream Job</span> </h1>
+        <div className='md:flex items-center mb-10 '>
+              <div className='md:w-1/2 px-2 md:py-14'>
+                   <h1 className='text-2xl md:text-6xl font-bold mb-5'>One Step <br /> Closer For Your <br /> <span className='text-blue-500'>Dream Job</span> </h1>
                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
                    <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
                    <button className='btn bg-blue-500 text-white hover:bg-green-500 mt-5'>Get Started</button>
               </div>
-              <div className='w-1/2'>
+              <div className='md:w-1/2'>
                 <img className='w-full h-96' src={bannerImg} alt="" />
               </div>
         </div>
