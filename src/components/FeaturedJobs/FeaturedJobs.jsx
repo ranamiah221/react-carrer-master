41fa8c8a6092e7202fe16fed9f8fe4decf1554e2
ranamiah221 <ranarasul21@gmail.com>
2024-10-14 16:26:15 +0600

    added show all jobs button and create dynamic data load functionality

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -3,17 +3,27 @@ import FeaturedJob from "../FeaturedJob/FeaturedJob";
 
 const FeaturedJobs = () => {
     const [featuredJobs, setFeaturedJobs]=useState([]);
+    const [dataLength, setDataLength]=useState(4);
     useEffect(()=>{
         fetch('jobs.json')
         .then(res=> res.json())
         .then(data=> setFeaturedJobs(data))
     },[])
     return (
-        <div className="grid grid-cols-2 gap-10 my-10">
+         <div>
+             <div className="grid grid-cols-2 gap-10 my-10">
             {
-                featuredJobs.map(featuredJob=> <FeaturedJob key={featuredJob.id} featuredJob={featuredJob}></FeaturedJob>)
+                featuredJobs.slice(0,dataLength).map(featuredJob=> <FeaturedJob key={featuredJob.id} featuredJob={featuredJob}></FeaturedJob>)
             }
-        </div>
+             </div>
+             <div className={dataLength === featuredJobs.length && "hidden"}>
+                   <div className="flex justify-center mb-10">
+                         <button onClick={()=>setDataLength(featuredJobs.length)} className="btn text-white bg-blue-500 hover:bg-green-500 ">See All Jobs</button>
+                    </div> 
+             </div>
+
+         </div>
+
     );
 };
 
